36f0e5e41ae00db27d30416771826a6f0157199e
0l3h <kubik7950@gmail.com>
2022-04-11 15:06:32 +0300

    Finished writing first dom ref example

--- src/components/Parent/index.jsx
+++ src/components/Parent/index.jsx
@@ -1,20 +1,22 @@
 import React, {useRef} from 'react';
-import Child from './../Child';
 
-function Container() {
+function Parent() {
     const inputRef = useRef(null);
 
     const focus = () => {
         inputRef.current.focus();
-    };
+    }
 
     return (
         <>
+            <h1>Example 1</h1>
+            <p>Click submit button to focus on input</p>
             <input type="text" ref={inputRef} /> 
             <button onClick={focus}>submit</button>
+
         </>
         
     );
 }
 
-export default Container;
+export default Parent;
