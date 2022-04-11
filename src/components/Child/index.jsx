8b82d3f273a8de8f2a31721759793826a1b17a56
0l3h <kubik7950@gmail.com>
2022-04-11 15:05:18 +0300

    Rewrote function component to class component

--- src/components/Child/index.jsx
+++ src/components/Child/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Child() {
-  return (
-    <div>
-        <input type="text"  /> 
-        
-    </div>
-  )
+export default class Child extends Component {
+  constructor(props) {
+    super(props);
+    this.count = React.createRef(0)
+  }
+
+  increment = () => {
+    this.count += 1;
+  };
+
+  render() {
+    return (
+      <div ref={this.count}></div>
+    )
+  }
 }
-
-export default Child
